6ccbc9760db399a9f6b257c1fde2c2b0a8906ab7
Balendra Sounthararajan <balendrs@stud.ntnu.no>
2022-10-17 12:08:31 +0200

    Added custom button to use in the app

---
+++ lib/common_widgets/custom_raised_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+/// Default button to use in the app
+class CustomRaisedButton extends StatelessWidget {
+  final Widget child;
+  final Color backgroundColor;
+  final double borderRadius;
+  final VoidCallback? onPressed;
+  final double height;
+
+  /// Creates an instance of button with custom
+  /// border radius and height.
+  const CustomRaisedButton({
+    Key? key,
+    required this.child,
+    required this.backgroundColor,
+    this.borderRadius = 2.0,
+    this.onPressed,
+    this.height = 50.0,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: backgroundColor,
+          elevation: 10,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(borderRadius),
+            ),
+          ),
+        ),
+        onPressed: onPressed,
+        child: child,
+      ),
+    );
+  }
+}
