f89d81699d2c598f72c0d444e087980870265e19
Balendra Sounthararajan <balendrs@stud.ntnu.no>
2022-11-10 11:35:42 +0100

    Refactored sections_page

--- lib/application/sections_page.dart
+++ lib/application/sections_page.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:kindermanager/application/section_start_page.dart';
+import 'package:kindermanager/common_widgets/bottom_model_content_widget.dart';
+import 'package:kindermanager/common_widgets/custom_bottom_navigation_bar.dart';
 import 'package:kindermanager/common_widgets/section_display_widget.dart';
 import 'package:kindermanager/services/firebase_database.dart';
 import 'package:provider/provider.dart';
@@ -14,8 +15,8 @@ import '../model/section.dart';
 import '../services/auth.dart';
 
 /// Homepage will be displayed after successful login,
-/// user will have options to select a section, create a new section
-/// or to logout.
+/// user will have options to select a section, create a new section,
+/// edit a section, delete a section or to logout.
 class SectionsPage extends StatefulWidget {
   const SectionsPage({Key? key}) : super(key: key);
 
@@ -36,10 +37,14 @@ class _SectionsPageState extends State<SectionsPage> {
   final imagePicker = ImagePicker();
   final fireStore = FirebaseStorage.instance.ref();
   File? image;
-  String imageUrl = "https://firebasestorage.googleapis.com/v0/b/kinder-manager.appspot.com/o/images%2Fjames%20webb%201.sep.jpeg?alt=media&token=e3c495af-e2a0-4165-ab46-2154014c6ad6";
-  bool isLoading = false;
 
+  /// Default image to display.
+  String imageUrl =
+      "https://firebasestorage.googleapis.com/v0/b/kinder-manager.appspot.com/o/images%2Fjames%20webb%201.sep.jpeg?alt=media&token=e3c495af-e2a0-4165-ab46-2154014c6ad6";
 
+  bool newImageSelected = false;
+
+  /// Content of the page.
   @override
   Widget build(BuildContext context) {
     final database = Provider.of<FirebaseDatabase>(context, listen: false);
@@ -69,6 +74,10 @@ class _SectionsPageState extends State<SectionsPage> {
       body: StreamBuilder<List<Section>>(
           stream: database.getSections(),
           builder: (context, snapshot) {
+            /// Displays a progress indicator if data is delayed
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
             if (snapshot.hasData) {
               return GridView.builder(
                 padding: const EdgeInsets.only(
@@ -79,13 +88,14 @@ class _SectionsPageState extends State<SectionsPage> {
                 ),
                 itemCount: snapshot.data?.length,
                 itemBuilder: (BuildContext context, int index) {
+                  /// Custom SectionDisplay widget to display sections.
                   return SectionDisplay(
                     section: snapshot.data![index],
                     onPressed: () {
                       _onSectionPressed(snapshot.data![index]);
                     },
-                    onLongPressed: () =>
-                        _sectionEdit(snapshot.data![index].id, database),
+                    onLongPressed: () => _sectionEdit(snapshot.data![index].id,
+                        snapshot.data![index].imageFile, database),
                   );
                 },
               );
@@ -94,91 +104,102 @@ class _SectionsPageState extends State<SectionsPage> {
           }),
 
       /// Bottom navigation bar with add new section and logout options.
-      bottomNavigationBar: BottomNavigationBar(
-        elevation: 10,
-        items: <BottomNavigationBarItem>[
-          /// Adding a new section icon to bottom navigation bar.
-          BottomNavigationBarItem(
-            icon: IconButton(
-                onPressed: () {
-                  final database =
-                      Provider.of<FirebaseDatabase>(context, listen: false);
-
-                  /// Creating a bottom model sheet to add a new section
-                  showModalBottomSheet<void>(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return Container(
-                        height: 200,
-                        color: Colors.lime,
-                        child: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              const Text('Add new section'),
-                              const SizedBox(
-                                height: 20,
-                              ),
-                              createForm(),
-                              const SizedBox(
-                                height: 20,
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  ElevatedButton(
-                                      child: const Text('Add image'),
-                                      onPressed: () {
-                                        pickImage();
-                                      }),
-                                  const SizedBox(
-                                    width: 20,
-                                  ),
-                                  isLoading
-                                      ? const CircularProgressIndicator()
-                                      : ElevatedButton(
-                                          child: const Text('Add section'),
-                                          onPressed: () {
-                                            _onSave(database);
-                                          }),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                },
-                icon: const Icon(Icons.add_box)),
-            label: 'New section',
-          ),
-
-          /// Adding Logout icon item to bottom navigation bar
-          BottomNavigationBarItem(
-            icon: IconButton(
-              onPressed: () {
-                final auth = Provider.of<Auth>(
-                  context,
-                  listen: false,
-                );
-
-                /// todo display a dialog box before signing out the user.
-                auth.signOut();
-              },
-              icon: const Icon(Icons.settings_applications_sharp),
-            ),
-            label: 'Logout',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.lightGreen,
-        onTap: _onItemTapped,
-        selectedFontSize: 16,
-        iconSize: 24,
+      bottomNavigationBar: CustomBottomNavigationBar(
+        createForm: createForm(),
+        onAddImage: pickImage,
+        onAddSection: _onAddSection,
+        onItemTapped: _onItemTapped,
+        onLogOut: () {
+          final auth = Provider.of<Auth>(
+            context,
+            listen: false,
+          );
+          auth.signOut();
+        },
+        selectedIndex: _selectedIndex,
       ),
     );
+  }
+
+  /// Updates the selected index
+  /// when clicked on the bottom icon.
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  /// Moves to selected Section start page.
+  void _onSectionPressed(Section section) {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) => SectionStartPage(section: section),
+      ),
+    );
+  }
+
+  /// Validating and saving new section to the firestore.
+  Future<void> _onAddSection() async {
+    if (_validateAndSaveForm()) {
+      final database = Provider.of<FirebaseDatabase>(context, listen: false);
+      final uniqueImageName = DateTime.now().millisecondsSinceEpoch.toString();
+      final ref = fireStore.child("images").child(uniqueImageName);
+      await ref.putFile(image!);
+      imageUrl = await ref.getDownloadURL();
+      final section = Section(imageFile: imageUrl, name: sectionName, id: '');
+      await database.createSection(section);
+      Navigator.pop(context);
+    }
+  }
+
+  /// Edits a existing section and updates the new values
+  void _sectionEdit(String docId, String imageUrl, FirebaseDatabase database) {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        /// Using custom widget to build bottom sheet.
+        return BottomModelContent(
+          titleText: "Manage section",
+          createForm: createForm(),
+          onChangeImage: () {
+            newImageSelected = true;
+            pickImage();
+          },
+          onDeleteSection: () {
+            _onDelete(database, docId);
+          },
+          onSaveSection: () {
+            _onEditUpload(database, docId, imageUrl);
+          },
+        );
+      },
+    );
+  }
+
+  /// Validating and editing a existing section.
+  Future<void> _onEditUpload(
+      FirebaseDatabase database, String docId, String imageUrl) async {
+    if (_validateAndSaveForm()) {
+      if (newImageSelected == true) {
+        final uniqueImageName =
+        DateTime.now().millisecondsSinceEpoch.toString();
+        final ref = fireStore.child("images").child(uniqueImageName);
+        await ref.putFile(image!);
+        // todo check for null and display alert dialog box
+        imageUrl = await ref.getDownloadURL();
+        newImageSelected = !newImageSelected;
+      }
+      final section =
+      Section(name: sectionName, id: docId, imageFile: imageUrl);
+      await database.editSection(section);
+      Navigator.pop(context);
+    }
+  }
+
+  /// Deletes the selected section.
+  Future<void> _onDelete(FirebaseDatabase database, String docId) async {
+    final section = Section(name: sectionName, id: docId, imageFile: imageUrl);
+    await database.deleteSection(section);
   }
 
   /// Creating a form to display in bottom sheet model
@@ -187,7 +208,7 @@ class _SectionsPageState extends State<SectionsPage> {
       key: formKey,
       child: Column(children: [
         TextFormField(
-          decoration: const InputDecoration(labelText: "Section name"),
+          decoration: const InputDecoration(labelText: " New section name"),
           validator: (value) {
             if (value!.isEmpty) {
               return "Please enter a valid section name";
@@ -210,126 +231,17 @@ class _SectionsPageState extends State<SectionsPage> {
     return false;
   }
 
-  /// Updates the selected index
-  /// when clicked on the bottom icon.
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  /// Validating and saving new section to the firestore.
-  Future<void> _onSave(FirebaseDatabase database) async {
-    if (_validateAndSaveForm()) {
-      final uniqueImageName = DateTime.now().millisecondsSinceEpoch.toString();
-     final ref = fireStore.child("images").child(uniqueImageName);
-          await ref.putFile(image!);
-      // todo check for null and display alert dialog box
-          imageUrl = await ref.getDownloadURL();
-      final section = Section(imageFile: imageUrl, name: sectionName, id: '');
-      await database.createSection(section);
-      Navigator.pop(context);
-    }
-  }
-
-  /// Validating and editing a existing section.
-  Future<void> _onEdit(FirebaseDatabase database, String docId) async {
-    if (_validateAndSaveForm()) {
-      final section =
-          Section(name: sectionName, id: docId, imageFile: imageUrl);
-      await database.editSection(section);
-      Navigator.pop(context);
-    }
-  }
-
-  /// Deletes the selected section.
-  Future<void> _onDelete(FirebaseDatabase database, String docId) async {
-    final section = Section(name: sectionName, id: docId, imageFile: imageUrl);
-    await database.deleteSection(section);
-  }
-
-  /// Moves to selected Section start page.
-  void _onSectionPressed(Section section) {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (context) => SectionStartPage(section: section),
-      ),
-    );
-  }
-
-  /// Edits a existing section and updates the new values
-  void _sectionEdit(String docId, FirebaseDatabase database) {
-    /// Creating a bottom model sheet to edit a section
-    showModalBottomSheet<void>(
-      context: context,
-      builder: (BuildContext context) {
-        return Container(
-          height: 200,
-          color: Colors.lime,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                const Text(
-                  'Edit section',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-
-                /// Text field to enter the section name.
-                createForm(),
-                const SizedBox(
-                  height: 20,
-                ),
-
-                /// Aligning buttons side by side.
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                        child: const Text('Delete '),
-                        onPressed: () {
-                          _onDelete(database, docId);
-                        }),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    ElevatedButton(
-                        child: const Text('Edit '),
-                        onPressed: () {
-                          _onEdit(database, docId);
-                        }),
-                  ],
-                )
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
-
   /// Picks image from the gallery.
   Future<void> pickImage() async {
     try {
-         final image = await imagePicker.pickImage(
-            source: ImageSource.gallery, imageQuality: 5);
+      final image = await imagePicker.pickImage(
+          source: ImageSource.gallery, imageQuality: 5);
       if (image == null) return;
       setState(() {
-        isLoading = true;
-              this.image = File(image.path);
+        this.image = File(image.path);
       });
     } on PlatformException catch (e) {
-      if (kDebugMode) {
-        print('Failed to pick image: $e');
-      }
-    } finally {
-      setState(() {
-        isLoading = false;
-      });
+      print('Failed to pick image: $e');
     }
   }
 //todo remove last selected image from gallery to avoid duplicate images.
