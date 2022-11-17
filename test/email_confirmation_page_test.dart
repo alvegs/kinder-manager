1b8b9e49a86a83161ade780b5ce85219f928f2f8
aleksveg <aleksveg@stud.ntnu.no>
2022-11-17 22:01:56 +0100

    tested that 2 CustomRaisedButtons were found in EmailConfirmationPage - test passed

---
+++ test/email_confirmation_page_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:kindermanager/application/sign_in/email_confirmation_page.dart';
+import 'package:kindermanager/custom_widgets/custom_button.dart';
+
+/// This class is for testing the widgets in email confirmation page class.
+/// The point is to see that the widgets contains the expected elements and
+/// are built correct.
+
+void main() {
+  testWidgets('description', (tester) async {
+    // for building and rendering the widget
+    await tester.pumpWidget(MaterialApp(home: const EmailConfirmationPage()),
+    );
+    final button = find.byType(CustomRaisedButton);
+    // expected to find 2 CustomRaisedButtons in EmailConf. widget
+    expect(button, findsNWidgets(2));
+  });
+}
