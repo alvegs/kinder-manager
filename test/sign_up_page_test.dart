e86838e0a9b669579685c698a3a7fac64a8c066f
aleksveg <aleksveg@stud.ntnu.no>
2022-12-01 14:06:56 +0100

    checking that text elements on sign up page are found with correct text - passes

--- test/sign_up_page_test.dart
+++ test/sign_up_page_test.dart
@@ -3,7 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:kindermanager/application/sign_in/sign_up_page.dart';
 import 'package:kindermanager/custom_widgets/custom_button.dart';
 
-/// Class serves as a tester for SignUpPage.
+/// Widget test for SignUpPage
+/// Checks that expected elements are found on the page
 
 void main() {
   testWidgets('', (WidgetTester tester) async {
@@ -14,6 +15,9 @@ void main() {
     expect(button, findsOneWidget);
     // check one clickable text is found
     expect(find.byType(TextButton), findsOneWidget);
+    expect(find.text("Sign up page"), findsOneWidget);
+    expect(find.text("Sign up"), findsNWidgets(2));
+    expect(find.text("Create an account"), findsOneWidget);
   });
 
   //TODO: implement test logic in this class
