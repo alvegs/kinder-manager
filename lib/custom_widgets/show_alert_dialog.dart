b23da8b7c0ae28b46626abb5b528d5373b2e868e
Balendra Sounthararajan <balendrs@stud.ntnu.no>
2022-12-01 09:25:47 +0100

    Changed custom alertbox to platform aware alertbox

--- lib/custom_widgets/show_alert_dialog.dart
+++ lib/custom_widgets/show_alert_dialog.dart
@@ -1,4 +1,7 @@
+import 'dart:io' show Platform;
+
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 /// Custom alert dialog.
 /// Returns the value chosen.
@@ -9,28 +12,56 @@ Future ShowAlertDialog(
   required String leftButtonText,
   required String rightButtonText,
 }) {
-  return showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-      actionsPadding: EdgeInsets.all(10),
-      title: Text(title),
-      content: Text(content),
-      actions: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(false),
+  if(Platform.isIOS ){
+    /// Displays IOS alert box
+    return showCupertinoModalPopup(
+        context: context,
+        builder: (context) => CupertinoAlertDialog(
+          title: Text(title),
+          content: Text(content),
+          actions: [
+            CupertinoDialogAction(
               child: Text(leftButtonText),
+              isDefaultAction: true,
+              onPressed: () => Navigator.of(context).pop(false),
             ),
-            SizedBox(width: 15),
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(true),
+            CupertinoDialogAction(
               child: Text(rightButtonText),
+              isDestructiveAction: true,
+              onPressed: () => Navigator.of(context).pop(true),
             ),
           ],
-        )
-      ],
-    ),
-  );
+        ),
+    );
+  }else{
+    /// Displays default alert box
+    return showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        actionsPadding: EdgeInsets.all(10),
+        title: Text(title),
+        content: Text(content),
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: Text(leftButtonText),
+              ),
+              SizedBox(width: 15),
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(true),
+                child: Text(rightButtonText, style: TextStyle(color: Colors.red)),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+
+  }
+
+
+
 }
