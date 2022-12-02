871988a5e615e547c0cd443892a579a265565df5
Joakim <joakimroren@gmail.com>
2022-12-02 15:37:59 +0100

    Added test class for custom elevated button

---
+++ test/custom_widgets/custom_elevated_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:kindermanager/custom_widgets/custom_elevated_button.dart';
+
+/// Testing the custom elevated button class
+void main() {
+  // Sets an predefines color to be used on the tests.
+  var color = Colors.indigo;
+  // Predefines the widget to prevent code duplication.
+  var widget = MaterialApp(
+      home: CustomElevatedButton(
+        status: 'tap me',
+        color: color,
+      )
+  );
+
+// Checks if a button can be found.
+  group('Custom Elevated Button tests', () {
+    testWidgets("button exist", (WidgetTester tester) async {
+      await tester.pumpWidget(widget);
+      final button = find.byType(CustomElevatedButton);
+      expect(button, findsOneWidget);
+      expect(find.byType(TextButton), findsNothing);
+    });
+// checks that there is a text in the widget.
+    testWidgets("text on button exist", (WidgetTester tester) async {
+      await tester.pumpWidget(widget);
+      expect(find.text('tap me'), findsOneWidget);
+    });
+// Checks that the color set is the one used.
+    testWidgets("color on button is correct", (WidgetTester tester) async {
+      await tester.pumpWidget(widget);
+      final button = tester
+          .widget<CustomElevatedButton>(find.byType(CustomElevatedButton));
+      expect(button.color, equals(Colors.indigo));
+    });
+  });
+
+
+}
