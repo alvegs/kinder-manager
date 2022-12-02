579f304c8b3991956e1a0c9d20251953cc45ae34
OdinUlvestad <79444794+OdinUlvestad@users.noreply.github.com>
2022-12-02 12:42:23 +0100

    Updated both bottom_model_content_widget and the design_themes constant values

--- lib/design_theme.dart
+++ lib/design_theme.dart
@@ -12,11 +12,15 @@ const fontSizeLarge  = 24.0;
 /** Commonly used font weights */
 const fontWeightBold = FontWeight.w500;
 
-/** Commonly used heights in sized-boxes */
+/** Commonly used heights and widths in sized-boxes */
 const heightSmall  = 10.0;
 const heightMedium = 20.0;
 
+const widthSmall = 10.0;
+const widthMedium = 20.0;
+
 /** Commonly used background colours */
+//ToDo: Name the variables better before putting them into the code
 final lightGreen50  = Colors.lightGreen[50];
 final lightGreen100 = Colors.lightGreen[100];
 final lightGreen300 = Colors.lightGreen[300];
